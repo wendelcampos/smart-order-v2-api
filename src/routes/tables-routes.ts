051c52a56ea75280c9236538412ab5f809561c58
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-10-01 15:30:39 -0300

    refactor: refactored tables routes

--- src/routes/tables-routes.ts
+++ src/routes/tables-routes.ts
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import { TablesController } from "@/controllers/tables-controller";
+import { ensureAuthenticated } from "@/middlewares/ensure-authenticated";
+import { verifyUserAuthorization } from "@/middlewares/verify-user-authorization";
 
 const tablesRoutes = Router()
 const tablesController = new TablesController()
+
+tablesRoutes.use(ensureAuthenticated, verifyUserAuthorization(["user"]))
 
 tablesRoutes.post("/", tablesController.create)
 tablesRoutes.put("/:id", tablesController.update)
