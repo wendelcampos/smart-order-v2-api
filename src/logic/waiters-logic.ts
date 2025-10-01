b2624b764963418a806d1fa7a3d0fc0d393a27b4
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-10-01 13:44:59 -0300

    feat: implemented waiters logic

---
+++ src/logic/waiters-logic.ts
@@ -0,0 +1,41 @@
+import { WaitersDTO } from "@/interfaces/WaitersDTO"
+import { WaitersRepository } from "@/repositories/implementations/waiters-repository"
+import { AppError } from "@/utils/AppError"
+
+class WaitersLogic {
+  private waitersRepository: WaitersRepository
+
+  constructor() {
+    this.waitersRepository = new WaitersRepository()
+  }
+  async create({ name, telephone }: WaitersDTO) {
+    await this.waitersRepository.save({
+      name,
+      telephone
+    })
+  }
+
+  async update({ name, telephone }: WaitersDTO, id: string) {
+    await this.waitersRepository.update({
+      name,
+      telephone
+    }, id)
+  }
+
+  async index() {
+    const waiters = await this.waitersRepository.index()
+
+    if(!waiters.length) {
+      throw new AppError("Nenhum Garçon encontrado!")
+    }
+
+    return waiters
+  }
+
+  async remove(id: string) {
+    await this.waitersRepository.remove(id)
+  }
+
+}
+
+export { WaitersLogic }
