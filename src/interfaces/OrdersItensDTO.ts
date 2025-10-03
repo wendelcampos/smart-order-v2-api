180d73bbc366a8e1c6ebad4a6ecfe976ae6a2bf3
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-10-03 20:46:58 -0300

    feat: implanted orderItens logic

---
+++ src/interfaces/OrdersItensDTO.ts
@@ -0,0 +1,5 @@
+export interface OrdersItensDTO {
+  orderId: string
+  quantity: number
+  productId: string
+}
---
+++ src/logic/ordersItens-logic.ts
@@ -0,0 +1,45 @@
+import { OrdersItensDTO } from "@/interfaces/OrdersItensDTO"
+import { OrdersItensRepository } from "@/repositories/implementations/ordersItens-repository"
+import { AppError } from "@/utils/AppError"
+
+class OrdersItensLogic {
+  private ordersItensRepository: OrdersItensRepository
+
+  constructor() {
+    this.ordersItensRepository = new OrdersItensRepository()
+  }
+
+  async create({ orderId, productId, quantity }: OrdersItensDTO) {
+    await this.ordersItensRepository.save({
+      orderId,
+      productId,
+      quantity
+    })
+  }
+
+  async index() {
+    const ordersItens = await this.ordersItensRepository.index()
+
+    if(!ordersItens.length) {
+      throw new AppError("Nenhum item encontrado")
+    }
+    
+    return ordersItens
+  }
+
+  async show(id: string) {
+    const orderItem = await this.ordersItensRepository.show(id)
+
+    if(!orderItem) {
+      throw new AppError("Nenhum item encontrado")
+    }
+
+    return orderItem
+  }
+
+  async remove(id: string) {
+    await this.ordersItensRepository.remove(id)
+  }
+}
+
+export { OrdersItensLogic }
