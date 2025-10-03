b09749d64a7943e0c81148a0a20e3ef952c1d9f0
Wendel Campos Aguiar <79800240+wendelcampos@users.noreply.github.com>
2025-10-03 20:47:13 -0300

    feat: implanted orderItens controller

---
+++ src/controllers/ordersItens-controller.ts
@@ -0,0 +1,53 @@
+import { Request, Response } from "express";
+import { OrdersItensLogic } from "@/logic/ordersItens-logic";
+import z from "zod";
+
+class OrdersItensController {
+  private ordersItensLogic: OrdersItensLogic
+
+  constructor() {
+    this.ordersItensLogic = new OrdersItensLogic()
+  }
+
+  create = async(request: Request, response: Response) => {
+    const bodySchema = z.object({
+      orderId: z.string(),
+      productId: z.string(),
+      quantity: z.number(), 
+    })
+
+    const { orderId, productId, quantity } = bodySchema.parse(request.body)
+
+    await this.ordersItensLogic.create({
+      orderId,
+      productId,
+      quantity
+    })
+
+    response.status(201).json()
+  }
+  
+  index = async (request: Request, response: Response) => {
+    const ordersItens = await this.ordersItensLogic.index()
+
+    response.json(ordersItens)
+  }
+
+  show = async (request: Request, response: Response) => {
+    const id = z.string().uuid({ message: "ID fornecido é inválido"}).parse(request.params.id)
+
+    const orderItem = await this.ordersItensLogic.show(id)
+
+    response.json(orderItem)
+  }
+
+  remove = async (request: Request, response: Response) => {
+    const id = z.string().uuid({ message: "ID fornecido é inválido"}).parse(request.params.id)
+    
+    await this.ordersItensLogic.remove(id)
+
+    response.json()
+  }
+}
+
+export { OrdersItensController }
